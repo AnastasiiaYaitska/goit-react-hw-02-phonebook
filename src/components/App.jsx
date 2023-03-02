a28615469ca4f3c3eed6bc4ae8f3d87eac39bd70
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-03-02 12:11:05 +0100

    add seving data in localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,32 @@ import { ContactsForm } from "./ContactForm/ContactForm";
 import { Contacts } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 import { Component } from "react";
+import contacts from "./contacts";
 
 
 export class App extends Component{
   state = {
-  contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+  contacts: [],
   filter:''
   }
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
 
+    if (savedContacts !== null) {
+      const unparsedSaveContacts = JSON.parse(savedContacts);
+      this.setState({contacts:unparsedSaveContacts })
+      return;
+    }
+    this.setState({contacts})
+  };
+
+  componentDidUpdate(nextProps, nextState) { 
+    if (nextState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  };
+  
 
   handlerFormSubmit = (name, number) => {
  
@@ -24,13 +38,11 @@ export class App extends Component{
       return alert( `${name} is already in the contacts ` )
     };
     
- 
     const contact = {
       id: nanoid(),
       name,
       number
     };
-
 
     this.setState(prevState=>({ contacts: [contact, ...prevState.contacts] }));
   };
